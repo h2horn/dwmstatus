8d6a1f254b3d433846fb1d7f3cea17161920b955
Hans-HarroHorn <h.h.horn@gmail.com>
2012-08-24 20:10:13 +0200

    dwmstatus cpu/bat/temp colors

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 dwmstatus: status.c
-	gcc -o dwmstatus status.c -lX11
+	gcc -Wall -pedantic -std=c99 -o dwmstatus status.c -lX11
 
 clean:
 	rm dwmstatus
--- status.c
+++ status.c
@@ -1,124 +1,144 @@
-#include <stdlib.h>
+/* made by profil 2011-12-29.
+**
+** Compile with:
+** gcc -Wall -pedantic -std=c99 -lX11 status.c
+*/
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <time.h>
 #include <X11/Xlib.h>
 
-// Below BATT_LOW% left on battery, the battery display turns red
-#define BATT_LOW	11
+static Display *dpy;
 
-// Above CPU_HI% cpu use, the cpu display turns red
-//	Note, likely due to hyperthreading or some other thing I'm still learning
-//  about, this can go well above "100%" during cpu-intensive processes.
-#define CPU_HI		50
+void setstatus(char *str) {
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
 
-// Sleeps for INTERVAL seconds between updates
-#define INTERVAL	1
+float getfreq(char *file) {
+	FILE *fd;
+	char *freq; 
+	float ret;
 
-// Files read for system info:
-#define CPU_FILE		"/proc/stat"
-#define MEM_FILE		"/proc/meminfo"
-//#define AUD_FILE		"/home/jmcclure/.status_info"
-#define BATT_NOW		"/sys/class/power_supply/BAT0/charge_now"
-#define BATT_FULL		"/sys/class/power_supply/BAT0/charge_full"
-#define BATT_STAT		"/sys/class/power_supply/BAT0/status"
-// Display format strings:
-//  Defaults make extensive use of escape characters for colors which require
-//  colorstatus patch.  There are also "extended" characters selected to work
-//  with terminus2 font for symbols and trianlge "flags".
-#define CPU_STR			"Ü	 Ï %d%% Ü"				// CPU percent when below CPU_HI%
-#define CPU_HI_STR		"Ü Ï %d%% Ü"				// CPU percent when above CPU_HI%
-#define MEM_STR			"Ü %d%% Ý %d%% Ý %ld%% Ü"	// memory, takes (up to) 3 integers: free, buffers, and cache
-#define VOL_STR			"Ü	 Ô %d% Ü"				// volume when not muted  IMPORTANT! SEE NOTE IN README FOR AUDO INFO
-#define VOL_MUTE_STR	"Ü	 Ô × Ü"					// volume when muted
-#define BAT_STR			"Ü	 %d%% Ü"				// Battery, unplugged, above BATT_LOW%
-#define BAT_LOW_STR		"Ü %d%% Ü"					// Battery, unplugged, below BATT_LOW% remaining
-#define BAT_CHRG_STR	"Ü %d%% Ü"					// Battery, when charging (plugged into AC)
-#define DATE_TIME_STR	"\uE015 Ü %a %b %d ÜÜ Õ %H:%M "	// This is a strftime format string which is passed localtime
-
-int main() {
-	Display *dpy;
-	Window root;
-	int num;
-	long jif1,jif2,jif3,jift;
-	long lnum1,lnum2,lnum3,lnum4;
-	char statnext[30], status[100];
-	time_t current;
-	FILE *infile;
-	// get initial jiffies
-	infile = fopen(CPU_FILE,"r");
-	fscanf(infile,"cpu %ld %ld %ld %ld",&jif1,&jif2,&jif3,&jift);
-	fclose(infile);
-	// Setup X display and root window id:
-	dpy=XOpenDisplay(NULL);
-	if ( dpy == NULL) {
-		fprintf(stderr, "ERROR: could not open display\n");
+	freq = malloc(10);
+	fd = fopen(file, "r");
+	if(fd == NULL) {
+		fprintf(stderr, "Cannot open '%s' for reading.\n", file);
 		exit(1);
 	}
-	root = XRootWindow(dpy,DefaultScreen(dpy));
-// MAIN LOOP STARTS HERE:
-	for (;;) {
-		status[0]='\0';
-	// CPU use:
-		infile = fopen(CPU_FILE,"r");
-		fscanf(infile,"cpu %ld %ld %ld %ld",&lnum1,&lnum2,&lnum3,&lnum4);
-		fclose(infile);
-		if (lnum4>jift)
-			num = (int) 100*(((lnum1-jif1)+(lnum2-jif2)+(lnum3-jif3))/(lnum4-jift));
-		else
-			num = 0;
-		jif1=lnum1; jif2=lnum2; jif3=lnum3; jift=lnum4;
-		if (num > CPU_HI)
-			sprintf(statnext,CPU_HI_STR,num);
-		else
-			sprintf(statnext,CPU_STR,num);
-		strcat(status,statnext);
-	// Memory use:
-		infile = fopen(MEM_FILE,"r");
-		fscanf(infile,"MemTotal: %ld kB\nMemFree: %ld kB\nBuffers: %ld kB\nCached: %ld kB\n",
-			&lnum1,&lnum2,&lnum3,&lnum4);
-		fclose(infile);
-		sprintf(statnext,MEM_STR,100*lnum2/lnum1,100*lnum3/lnum1,100*lnum4/lnum1);
-		strcat(status,statnext);
-	/*/ Audio volume:
-		infile = fopen(AUD_FILE,"r");
-		fscanf(infile,"%d",&num);
-		fclose(infile);
-		if (num == -1)
-			sprintf(statnext,VOL_MUTE_STR,num);
-		else
-			sprintf(statnext,VOL_STR,num);
-		strcat(status,statnext);*/
-	// Power / Battery:
-		infile = fopen(BATT_NOW,"r");
-			fscanf(infile,"%ld\n",&lnum1);fclose(infile);
-		infile = fopen(BATT_FULL,"r");
-			fscanf(infile,"%ld\n",&lnum2);fclose(infile);
-		infile = fopen(BATT_STAT,"r");
-			fscanf(infile,"%s\n",statnext);fclose(infile);
-		num = lnum1*100/lnum2;
-		if (strncmp(statnext,"Charging",8) == 0) {
-			sprintf(statnext,BAT_CHRG_STR,num);
-		}
-		else {
-			if (num < BATT_LOW)
-				sprintf(statnext,BAT_LOW_STR,num);
-			else
-				sprintf(statnext,BAT_STR,num);
-		}
-		strcat(status,statnext);
-	// Date & Time:
-		time(&current);
-		strftime(statnext,38,DATE_TIME_STR,localtime(&current));
-		strcat(status,statnext);
-	// Set root name
-		XStoreName(dpy,root,status);
-		XFlush(dpy);
-		sleep(INTERVAL);
+
+	fgets(freq, 10, fd);
+	fclose(fd);
+
+	ret = atof(freq)/1000000;
+	free(freq);
+	return ret;
+}
+
+char *getdatetime() {
+	char *buf;
+	time_t result;
+	struct tm *resulttm;
+
+	if((buf = malloc(sizeof(char)*65)) == NULL) {
+		fprintf(stderr, "Cannot allocate memory for buf.\n");
+		exit(1);
 	}
-// NEXT LINES SHOULD NEVER EXECUTE, only here to satisfy my O.C.D.
+	result = time(NULL);
+	resulttm = localtime(&result);
+	if(resulttm == NULL) {
+		fprintf(stderr, "Error getting localtime.\n");
+		exit(1);
+	}
+	if(!strftime(buf, sizeof(char)*65-1, "%a %b %d %H:%M:%S", resulttm)) {
+		fprintf(stderr, "strftime is 0.\n");
+		exit(1);
+	}
+	
+	return buf;
+}
+
+int getbattery() {
+	FILE *fd;
+	int energy_now, energy_full, voltage_now;
+
+	fd = fopen("/sys/class/power_supply/BAT0/charge_now", "r");
+	if(fd == NULL) {
+		fprintf(stderr, "Error opening charge_now.\n");
+		return -1;
+	}
+	fscanf(fd, "%d", &energy_now);
+	fclose(fd);
+
+
+	fd = fopen("/sys/class/power_supply/BAT0/charge_full", "r");
+	if(fd == NULL) {
+		fprintf(stderr, "Error opening charge_full.\n");
+		return -1;
+	}
+	fscanf(fd, "%d", &energy_full);
+	fclose(fd);
+
+
+	fd = fopen("/sys/class/power_supply/BAT0/voltage_now", "r");
+	if(fd == NULL) {
+		fprintf(stderr, "Error opening voltage_now.\n");
+		return -1;
+	}
+	fscanf(fd, "%d", &voltage_now);
+	fclose(fd);
+
+
+	return ((float)energy_now * 1000 / (float)voltage_now) * 100 / ((float)energy_full * 1000 / (float)voltage_now);
+}
+
+int gettemperature() {
+    FILE *fd;
+    int temp;
+
+    fd = fopen("/sys/class/hwmon/hwmon0/temp1_input", "r");
+    if(fd == NULL) {
+        fprintf(stderr, "Error opening temp1_input.\n");
+        return -1;
+    }
+    fscanf(fd, "%d", &temp);
+    fclose(fd);
+
+    return (temp / 1000);
+}
+
+int main(void) {
+	char *status;
+	float cpu0, cpu1;
+	char *datetime;
+	int bat0, temp;
+
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "Cannot open display.\n");
+		return 1;
+	}
+
+	if((status = malloc(200)) == NULL)
+		exit(1);
+	
+
+	for (;;sleep(1)) {
+		cpu0 = getfreq("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
+		cpu1 = getfreq("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq");
+		datetime = getdatetime();
+		bat0 = getbattery();
+        temp = gettemperature();
+		snprintf(status, 200, "\x02 %0.2f, %0.2f \x01|\x02 B %d%% \x01|\x02 T %d C \x01|\x02 %s", cpu0, cpu1, bat0, temp, datetime);
+
+		free(datetime);
+		setstatus(status);
+	}
+
+	free(status);
 	XCloseDisplay(dpy);
+
 	return 0;
 }
 

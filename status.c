c9fd807b61a6f7a79c86153cfff2bf3fc64bb1b9
Hans-HarroHorn <h.h.horn@gmail.com>
2012-08-30 01:55:35 +0200

    dwmstatus rework

--- status.c
+++ status.c
@@ -6,133 +6,179 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <time.h>
 #include <X11/Xlib.h>
 
 static Display *dpy;
+long cpu0_work = 0;
+long cpu0_total = 0;
+long cpu1_work = 0;
+long cpu1_total = 0;
 
 void setstatus(char *str) {
 	XStoreName(dpy, DefaultRootWindow(dpy), str);
 	XSync(dpy, False);
 }
 
-float getfreq(char *file) {
-	FILE *fd;
-	char *freq; 
-	float ret;
+int getcpu(char *status, size_t size) {
+    FILE *fd;
+    long jif1, jif2, jif3, jif4, jif5, jif6, jif7;
+    long work0, total0, work1, total1;
+    int load0, load1, freq;
+    char *color0, *color1;
 
-	freq = malloc(10);
-	fd = fopen(file, "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Cannot open '%s' for reading.\n", file);
-		exit(1);
-	}
+    // ---- LOAD
+    fd = fopen("/proc/stat", "r");
+    char c;
+    while (c != '\n') c = fgetc(fd);
+    fscanf(fd, "cpu0 %ld %ld %ld %ld %ld %ld %ld", &jif1, &jif2, &jif3, &jif4, &jif5, &jif6, &jif7);
+    work0 = jif1 + jif2 + jif3;
+    total0 = work0 + jif4 + jif5 + jif6 + jif7;
 
-	fgets(freq, 10, fd);
-	fclose(fd);
+    c = 0;
+    while (c != '\n') c = fgetc(fd);
+    fscanf(fd, "cpu1 %ld %ld %ld %ld %ld %ld %ld", &jif1, &jif2, &jif3, &jif4, &jif5, &jif6, &jif7);
+    work1 = jif1 + jif2 + jif3;
+    total1 = work1 + jif4 + jif5 + jif6 + jif7;
 
-	ret = atof(freq)/1000000;
-	free(freq);
-	return ret;
+    fclose(fd);
+
+    load0 = 100 * (work0 - cpu0_work) / (total0 - cpu0_total);
+    load1 = 100 * (work1 - cpu1_work) / (total1 - cpu1_total);
+    
+    cpu0_work = work0;
+    cpu0_total = total0;
+    cpu1_work = work1;
+    cpu1_total = total1;
+
+    // ---- FREQ
+    fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", "r");
+    fscanf(fd, "%d", &freq);
+    fclose(fd);
+    if(freq <= 800000) {
+        color0 = "\x02";
+    } else if(freq >= 2000000) {
+        color0 = "\x03";
+    } else {
+        color0 = "\x05";
+    }
+
+    fd = fopen("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq", "r");
+    fscanf(fd, "%d", &freq);
+    fclose(fd);
+    if(freq <= 800000) {
+        color1 = "\x02";
+    } else if(freq >= 2000000) {
+        color1 = "\x03";
+    } else {
+        color1 = "\x05";
+    }
+
+    return snprintf(status, size, "\x02\uE026 %s%d%% %s%d%%", color0, load0, color1, load1);
 }
 
-char *getdatetime() {
-	char *buf;
+int getmem(char *status, size_t size) {
+    FILE *fd;
+    long total, free, buf, cache;
+    int used;
+
+    fd = fopen("/proc/meminfo", "r");
+    fscanf(fd, "MemTotal: %ld kB\nMemFree: %ld kB\nBuffers: %ld kB\nCached: %ld kB\n", &total, &free, &buf, &cache);
+    fclose(fd);
+    used = 100 * (total - free - buf - cache) / total;
+    if(used > 80) {
+        return snprintf(status, size, "\x02\uE021 \x03%d%%", used);
+    } else {
+
+        return snprintf(status, size, "\x02\uE021 %d%%", used);
+    }
+}
+
+int getdatetime(char *status, size_t size) {
 	time_t result;
 	struct tm *resulttm;
 
-	if((buf = malloc(sizeof(char)*65)) == NULL) {
-		fprintf(stderr, "Cannot allocate memory for buf.\n");
-		exit(1);
-	}
 	result = time(NULL);
 	resulttm = localtime(&result);
-	if(resulttm == NULL) {
-		fprintf(stderr, "Error getting localtime.\n");
-		exit(1);
-	}
-	if(!strftime(buf, sizeof(char)*65-1, "%a %b %d %H:%M:%S", resulttm)) {
-		fprintf(stderr, "strftime is 0.\n");
-		exit(1);
-	}
 	
-	return buf;
+    return strftime(status, size, "\x02\uE016 %a %b %d %H:%M", resulttm);
 }
 
-int getbattery() {
+int getbattery(char *status, size_t size) {
 	FILE *fd;
-	int energy_now, energy_full, voltage_now;
+	int now, full, bat;
+    char stat[12];
 
 	fd = fopen("/sys/class/power_supply/BAT0/charge_now", "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Error opening charge_now.\n");
-		return -1;
-	}
-	fscanf(fd, "%d", &energy_now);
+	fscanf(fd, "%d", &now);
 	fclose(fd);
 
 
 	fd = fopen("/sys/class/power_supply/BAT0/charge_full", "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Error opening charge_full.\n");
-		return -1;
-	}
-	fscanf(fd, "%d", &energy_full);
+	fscanf(fd, "%d", &full);
 	fclose(fd);
 
-
-	fd = fopen("/sys/class/power_supply/BAT0/voltage_now", "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Error opening voltage_now.\n");
-		return -1;
-	}
-	fscanf(fd, "%d", &voltage_now);
+	fd = fopen("/sys/class/power_supply/BAT0/status", "r");
+	fscanf(fd, "%s", stat);
 	fclose(fd);
 
+    bat = 100 * now / full;
 
-	return ((float)energy_now * 1000 / (float)voltage_now) * 100 / ((float)energy_full * 1000 / (float)voltage_now);
+    if(strncmp(stat, "Discharging", 11) == 0) {    
+        if(bat < 20) {            
+            return snprintf(status, size, "\x03\uE03D %d%%", bat);
+        } else if(bat > 80) {
+            return snprintf(status, size, "\x04\uE03F %d%%", bat);
+        } else {
+            return snprintf(status, size, "\x05\uE03E %d%%", bat);
+        }
+    } else if(strncmp(stat, "Charging", 8) == 0) {
+        return snprintf(status, size, "\x02\uE043 %d%%", bat);
+    } else {
+        return snprintf(status, size, "\x02\uE043");
+    }
 }
 
-int gettemperature() {
+int gettemp(char *status, size_t size) {
     FILE *fd;
     int temp;
 
     fd = fopen("/sys/class/hwmon/hwmon0/temp1_input", "r");
-    if(fd == NULL) {
-        fprintf(stderr, "Error opening temp1_input.\n");
-        return -1;
-    }
     fscanf(fd, "%d", &temp);
     fclose(fd);
+    temp /= 1000;
 
-    return (temp / 1000);
+    if(temp > 60) {
+        return snprintf(status, size, "\x02\uE0CF \x03%d°C", temp);
+    } else if(temp > 40) {
+        return snprintf(status, size, "\x02\uE0CF \x05%d°C", temp);
+    } else {
+        return snprintf(status, size, "\x02\uE0CF %d°C", temp);
+    }
 }
 
 int main(void) {
-	char *status;
-	float cpu0, cpu1;
-	char *datetime;
-	int bat0, temp;
-
+	char status[200];
+    int l = 0;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "Cannot open display.\n");
 		return 1;
 	}
 
-	if((status = malloc(200)) == NULL)
-		exit(1);
-	
+	for (;;sleep(2)) {
+        
+        l = getcpu(status, sizeof(status));
+        l += snprintf(status + l, sizeof(status) - l, "\x01 | ");
+        l += gettemp(status + l, sizeof(status) - l);
+        l += snprintf(status + l, sizeof(status) - l, "\x01 | ");
+        l += getmem(status + l, sizeof(status) - l);
+        l += snprintf(status + l, sizeof(status) - l, "\x01 | ");
+        l += getbattery(status + l, sizeof(status) - l);
+        l += snprintf(status + l, sizeof(status) - l, "\x01 | ");
+        l += getdatetime(status + l, sizeof(status) - l);
 
-	for (;;sleep(1)) {
-		cpu0 = getfreq("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
-		cpu1 = getfreq("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq");
-		datetime = getdatetime();
-		bat0 = getbattery();
-        temp = gettemperature();
-		snprintf(status, 200, "\x02 %0.2f, %0.2f \x01|\x02 B %d%% \x01|\x02 T %d C \x01|\x02 %s", cpu0, cpu1, bat0, temp, datetime);
-
-		free(datetime);
 		setstatus(status);
 	}
 

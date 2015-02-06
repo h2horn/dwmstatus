235fc96641a37daaef08e855ebe8a2f4f006826b
Hans-HarroHorn <h.h.horn@gmail.com>
2015-02-06 12:38:42 +0100

    add gpu temp, cleanup

--- status.c
+++ status.c
@@ -1,5 +1,5 @@
 /*
- * This is free and unencumbered public domain software. 
+ * This is free and unencumbered public domain software.
  * For more information see http://unlicense.org/.
  *
  * @author Hans-Harro Horn
@@ -19,8 +19,20 @@ long cpu1_work = 0;
 long cpu1_total = 0;
 
 void setstatus(char *str) {
-	XStoreName(dpy, DefaultRootWindow(dpy), str);
-	XSync(dpy, False);
+    XStoreName(dpy, DefaultRootWindow(dpy), str);
+    XSync(dpy, False);
+}
+
+int readInt(char *input) {
+    FILE *fd;
+    int val;
+
+    fd = fopen(input, "r");
+    if (fd==NULL)
+        return 0;
+    fscanf(fd, "%d", &val);
+    fclose(fd);
+    return val;
 }
 
 int getcpu(char *status, size_t size) {
@@ -48,34 +60,28 @@ int getcpu(char *status, size_t size) {
 
     load0 = 100 * (work0 - cpu0_work) / (total0 - cpu0_total);
     load1 = 100 * (work1 - cpu1_work) / (total1 - cpu1_total);
-    
+
     cpu0_work = work0;
     cpu0_total = total0;
     cpu1_work = work1;
     cpu1_total = total1;
 
     // ---- FREQ
-    fd = fopen("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", "r");
-    fscanf(fd, "%d", &freq);
-    fclose(fd);
-    if(freq <= 800000) {
+    freq = readInt("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq");
+    if(freq <= 800000)
         color0 = "\x02";
-    } else if(freq >= 2000000) {
+    else if(freq >= 2000000)
         color0 = "\x06";
-    } else {
+    else
         color0 = "\x04";
-    }
 
-    fd = fopen("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq", "r");
-    fscanf(fd, "%d", &freq);
-    fclose(fd);
-    if(freq <= 800000) {
+    freq = readInt("/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq");
+    if(freq <= 800000)
         color1 = "\x02";
-    } else if(freq >= 2000000) {
+    else if(freq >= 2000000)
         color1 = "\x06";
-    } else {
+    else
         color1 = "\x04";
-    }
 
     return snprintf(status, size, "\x09""C%s%3d%%%s%3d%%", color0, load0, color1, load1);
 }
@@ -99,36 +105,36 @@ int getmem(char *status, size_t size) {
 }
 
 int getdatetime(char *status, size_t size) {
-	time_t result;
-	struct tm *resulttm;
+    time_t result;
+    struct tm *resulttm;
 
-	result = time(NULL);
-	resulttm = localtime(&result);
-	
+    result = time(NULL);
+    resulttm = localtime(&result);
+
     return strftime(status, size, "\x09""D\x02%b %d %H:%M", resulttm);
 }
 
 int getbattery(char *status, size_t size) {
-	FILE *fd;
-	int now, full, bat;
+    FILE *fd;
+    int now, full, bat;
     char stat[12];
 
-	fd = fopen("/sys/class/power_supply/BAT0/charge_now", "r");
-	fscanf(fd, "%d", &now);
-	fclose(fd);
+    fd = fopen("/sys/class/power_supply/BAT0/charge_now", "r");
+    fscanf(fd, "%d", &now);
+    fclose(fd);
 
-	fd = fopen("/sys/class/power_supply/BAT0/charge_full", "r");
-	fscanf(fd, "%d", &full);
-	fclose(fd);
+    fd = fopen("/sys/class/power_supply/BAT0/charge_full", "r");
+    fscanf(fd, "%d", &full);
+    fclose(fd);
 
-	fd = fopen("/sys/class/power_supply/BAT0/status", "r");
-	fscanf(fd, "%s", stat);
-	fclose(fd);
+    fd = fopen("/sys/class/power_supply/BAT0/status", "r");
+    fscanf(fd, "%s", stat);
+    fclose(fd);
 
     bat = 100 * now / full;
 
-    if(strncmp(stat, "Discharging", 11) == 0) {    
-        if(bat < 20) {            
+    if(strncmp(stat, "Discharging", 11) == 0) {
+        if(bat < 20) {
             return snprintf(status, size, "\x09""B\x06%d%%", bat);
         } else if(bat > 80) {
             return snprintf(status, size, "\x09""B\x0b%d%%", bat);
@@ -143,33 +149,33 @@ int getbattery(char *status, size_t size) {
 }
 
 int gettemp(char *status, size_t size) {
-    FILE *fd;
-    int temp;
+    int cpu, gpu;
+    char *colorc, *colorg;
 
-    fd = fopen("/sys/class/hwmon/hwmon0/temp1_input", "r");
-    fscanf(fd, "%d", &temp);
-    fclose(fd);
-    temp /= 1000;
+    cpu = readInt("/sys/class/hwmon/hwmon1/temp1_input") / 1000;
+    gpu = readInt("/sys/class/hwmon/hwmon0/temp1_input") / 1000;
 
-    if(temp > 60) {
-        return snprintf(status, size, "\x09""T\x06%d℃", temp);
-    } else if(temp > 45) {
-        return snprintf(status, size, "\x09""T\x04%d℃", temp);
-    } else {
-        return snprintf(status, size, "\x09""T\x02%d℃", temp);
-    }
+    if (cpu > 60)
+        colorc = "\x06";
+    else if(cpu > 45)
+        colorc = "\x04";
+    else
+        colorc = "\x02";
+
+    if (gpu > 95)
+        colorg = "\x06";
+    else if (gpu > 75)
+        colorg = "\x04";
+    else
+        colorg = "\x02";
+
+    return snprintf(status, size, "\x09""T%s%d°C%s%d°C", colorc, cpu, colorg, gpu);
 }
 
 int getvol(char* status, size_t size) {
-    FILE *fd;
     int vol;
 
-    fd = fopen("/tmp/volume", "r");
-    if (fd==NULL) {
-        return 0;
-    }
-    fscanf(fd, "%d", &vol);
-    fclose(fd);
+    vol = readInt("/tmp/volume");
 
     if(vol == 0) {
         return snprintf(status, size, "\x09""V\x02""Mute");
@@ -200,18 +206,17 @@ int getwired(char* status, size_t size) {
 }
 
 int main(void) {
-	char status[100];
+    char status[100];
     int l = 0;
 
-	if (!(dpy = XOpenDisplay(NULL))) {
-		fprintf(stderr, "Cannot open display.\n");
-		return 1;
-	}
+    if (!(dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "Cannot open display.\n");
+        return 1;
+    }
 
-	for (;;sleep(2)) {
-        
+    for (;;sleep(2)) {
+
         l = getcpu(status, sizeof(status));
-        //l += snprintf(status + l, sizeof(status) - l, "\x01|");
         l += gettemp(status + l, sizeof(status) - l);
         l += getmem(status + l, sizeof(status) - l);
         l += getwireless(status + l, sizeof(status) - l);
@@ -219,15 +224,13 @@ int main(void) {
         l += getbattery(status + l, sizeof(status) - l);
         l += getvol(status + l, sizeof(status) - l);
         l += getdatetime(status + l, sizeof(status) - l);
-        
-        //fprintf(stderr, "%d\n", l);
 
-		setstatus(status);
-	}
+        setstatus(status);
+    }
 
-	free(status);
-	XCloseDisplay(dpy);
+    free(status);
+    XCloseDisplay(dpy);
 
-	return 0;
+    return 0;
 }
 
